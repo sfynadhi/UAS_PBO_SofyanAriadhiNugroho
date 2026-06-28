9efe6aadc5ae7857a5d7362dfb83553144439fe9
sfynadhi <adhinugrohosofyan@gmail.com>
2026-06-28 23:10:27 +0700

    p

--- koneksi.php
+++ koneksi.php
@@ -3,7 +3,7 @@
 class Koneksi
 {
     private $host = "localhost";
-    private $username = "root";
+    private $user = "root";
     private $password = "";
     private $database = "db_uas_pbo_trpl1a_sofyanapriadhinugroho";
 
@@ -13,14 +13,16 @@ class Koneksi
     {
         $this->conn = mysqli_connect(
             $this->host,
-            $this->username,
+            $this->user,
             $this->password,
             $this->database
         );
 
         if (!$this->conn) {
-            die("Koneksi Database Gagal : " . mysqli_connect_error());
+            die("Koneksi gagal : " . mysqli_connect_error());
         }
+
+        mysqli_set_charset($this->conn, "utf8");
     }
 
     public function getConnection()
@@ -28,5 +30,3 @@ class Koneksi
         return $this->conn;
     }
 }
-
-?>
